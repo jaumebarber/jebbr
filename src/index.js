ff1b9cab85ca3906d0ecf2982c7adae2bf54241c
jaumebarber <jaumebarber@gmail.com>
2019-11-01 12:28:49 +0100

    test suite set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app/App'
+import App from './components/App/App'
 
 render(<App />, document.getElementById('root'))
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!')
+    printMe()
+  })
+}
