e8e43766c1b234f6ea571b3ee2b60ff1b97ff1ec
jaumebarber <jaumebarber@gmail.com>
2019-11-05 12:31:28 +0100

    environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,100 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  entry: {
-    bundle: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(scss|sass)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
+module.exports = env => {
+  const isDevelopment = env.NODE_ENV === 'development'
+  return {
+    entry: {
+      bundle: './src/index.js'
+    },
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'inline-source-map' : '',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: ['babel-loader', 'eslint-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader']
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: ['file-loader']
+        },
+        {
+          test: /\.styles\.s(a|c)ss$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: isDevelopment ? false : true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
             }
-          }
-        ]
+          ]
+        },
+        {
+          test: /\.(scss|sass)$/,
+          exclude: /\.styles.(s(a|c)ss)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: isDevelopment ? false : true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        }
+      ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx', '.scss'] },
+    output: {
+      filename: '[name].[hash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      contentBase: './dist',
+      port: 3000,
+      hot: true,
+      compress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'jebbr.css',
+        chunkFilename: '[id].css'
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
       }
-    ]
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: './dist',
-    port: 3000,
-    hot: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles/jebbr.css'
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
     }
   }
 }
